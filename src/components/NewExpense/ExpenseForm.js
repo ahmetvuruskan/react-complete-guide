bb95b1b3164c6ffc59919ac7b192bbeaec8ced4c
Ahmet Vuruskan <ahmetvrskn@gmail.com>
2023-06-29 11:41:52 +0300

    handling form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,49 @@
-import React ,{useState} from 'react';
+import React, {useState} from 'react';
 import "./../Css/NewExpense/ExpenseForm.css";
 
 const ExpenseForm = () => {
-   const [enteredTitle,setEnteredTitle] = useState("")
-    const titleChangeHandler = (event) =>{
-        setEnteredTitle(event.target.value)
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    })
+    const titleChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value,
+        })
     }
-    const [enteredAmount,setEnteredAmount] = useState("")
-    const amountChangeHandler = (event) =>{
-        setEnteredAmount(event.target.value)
+
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.target.value,
+        })
+
     }
-    const [enteredDate,setEnteredDate] = useState("")
-    const dateChangeHandler = (event) =>{
-        setEnteredDate(event.target.value)
+
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        })
+    }
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: userInput.enteredTitle,
+            amount: userInput.enteredAmount,
+            date: userInput.enteredDate
+        }
+        console.log(expenseData);
+
     }
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} ></input>
+                    <input type="text" onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
