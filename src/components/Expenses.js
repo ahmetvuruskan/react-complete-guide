c4e7f788127260a4b4de84603419f430a714ba9c
ahmetvuruskan <ahmetvrskn@gmail.com>
2023-08-27 14:51:24 +0300

    refactor : refactored expenses list single component to multi component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./Filter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -13,22 +14,11 @@ function Expenses(props) {
     const filteredExpenses = props.expenses.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-
     return (
         <div className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilteredyear={changeFilteredYear}/>
             <div className="expenses-list">
-                {
-                    filteredExpenses.map((expense) => (
-                            <ExpenseItem
-                                key={expense.id}
-                                title={expense.title}
-                                amount={expense.amount}
-                                date={expense.date}
-                            />
-                        )
-                    )
-                }
+               <ExpensesList items={filteredExpenses}/>
             </div>
         </div>
     );
